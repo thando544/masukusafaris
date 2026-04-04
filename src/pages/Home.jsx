04bd29ab5372587468dfdbcfdb6f9330a33207ea
thando mpofu <mpofu7085@gmail.com>
2026-04-04 11:48:47 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,22 +3,24 @@ import Footer from "../components/layout/Footer";
 import HeroSection from "../components/home/HeroSection";
 import AboutPreview from "../components/home/AboutPreview";
 import ActivitiesPreview from "../components/home/ActivitiesPreview";
-import StatsSection from "../components/home/StatsSection";
 import PackagesPreview from "../components/home/PackagesPreview";
+import TransfersPreview from "../components/home/TransfersPreview";
 import CTASection from "../components/home/CTASection";
 
 export default function Home() {
   return (
     <div className="bg-white text-zinc-900">
       <Navbar />
+
       <main>
         <HeroSection />
         <AboutPreview />
         <ActivitiesPreview />
-        <StatsSection />
         <PackagesPreview />
+        <TransfersPreview />
         <CTASection />
       </main>
+
       <Footer />
     </div>
   );
