bcbe3b745fa646b35a84acd027a25c8125a2d646
thando mpofu <mpofu7085@gmail.com>
2026-04-04 11:55:54 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Activities from "./pages/Activities";
 import ActivityDetails from "./pages/ActivityDetails";
+import Packages from "./pages/Packages"; 
 import PackageDetails from "./pages/PackageDetails";
 import Contact from "./pages/Contact";
 import BookingPage from "./pages/BookingPage";
@@ -13,9 +14,11 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/activities" element={<Activities />} />
-        <Route path="/about" element={<About />} />
         <Route path="/activities/:slug" element={<ActivityDetails />} />
+        <Route path="/packages" element={<Packages />} />
+
         <Route path="/packages/:slug" element={<PackageDetails />} />
+        <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/book" element={<BookingPage />} />
       </Routes>
