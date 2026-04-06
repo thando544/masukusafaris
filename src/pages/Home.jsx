aa1ada30f54828ff087f205627e9fbe4b097dcc1
thando mpofu <mpofu7085@gmail.com>
2026-04-06 14:56:01 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,7 +17,6 @@ export default function Home() {
         <AboutPreview />
         <ActivitiesPreview />
         <PackagesPreview />
-        <TransfersPreview />
         <CTASection />
       </main>
 
